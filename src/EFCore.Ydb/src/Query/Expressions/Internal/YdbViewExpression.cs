198413a62b7ace069c7ea15e84c42c0c7dc19b19
Ivan Travnikov <travnikov2014@yandex.ru>
2025-12-21 16:42:57 +0300

    template

---
+++ src/EFCore.Ydb/src/Query/Expressions/Internal/YdbViewExpression.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore.Query;
+using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace EntityFrameworkCore.Ydb.Query.Expressions.Internal;
+
+public class YdbViewExpression(SqlExpression index)
+    : SqlExpression(typeof(bool), null)
+{
+    public override Expression Quote() => throw new NotImplementedException();
+
+    protected override void Print(ExpressionPrinter expressionPrinter) => throw new NotImplementedException();
+    
+    protected override Expression VisitChildren(ExpressionVisitor visitor) => throw new NotImplementedException();
+    
+    public override bool Equals(object? obj) => throw new NotImplementedException();
+    
+    public bool Equals(YdbViewExpression? other) => throw new NotImplementedException();
+    
+    public override int GetHashCode() => throw new NotImplementedException();
+}
