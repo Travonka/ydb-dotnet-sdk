742d1ac8591bed33ffaa4167381b3145f231fc9e
Travonka0 <travnikov2014@yandex.ru>
2025-12-23 02:34:50 +0300

    wp

--- src/EFCore.Ydb/src/Query/Expressions/Internal/YdbViewExpression.cs
+++ src/EFCore.Ydb/src/Query/Expressions/Internal/YdbViewExpression.cs
@@ -16,21 +16,52 @@ public class YdbViewExpression(string name,
     public string Name { get; } = name;
     public string? Schema { get; } = schema;
     public string IndexName { get; } = indexName;
-    public override TableExpressionBase Clone(string? alias, ExpressionVisitor cloningExpressionVisitor) => throw new NotImplementedException();
+    public override TableExpressionBase Clone(string? alias, ExpressionVisitor cloningExpressionVisitor) => 
+        new YdbIndexTableExpression(Name, Schema, IndexName, alias);
 
-    public override TableExpressionBase WithAlias(string newAlias) => throw new NotImplementedException();
+    public override TableExpressionBase WithAlias(string newAlias) => ReferenceEquals(newAlias, Alias) ?
+     this : new YdbIndexTableExpression(Name, Schema, IndexName, newAlias);
 
-    public override Expression Quote() => throw new NotImplementedException();
+    public override bool Equals(object? obj)
+        => obj is YdbIndexTableExpression other && Equals(other);
 
-    protected override void Print(ExpressionPrinter expressionPrinter) => throw new NotImplementedException();
+    [Experimental("EF9100")]
+    public override Expression Quote()
+        => New(
+            _quotingConstructor ??= typeof(YdbIndexTableExpression).GetConstructor(
+                [typeof(string), typeof(string), typeof(string), typeof(string)])!,
+            Constant(Name),
+            Constant(Schema, typeof(string)),
+            Constant(IndexName),
+            Constant(Alias, typeof(string)));
+
+    protected override void Print(ExpressionPrinter expressionPrinter) {
+        if (!string.IsNullOrEmpty(Schema))
+        {
+            expressionPrinter.Append(Schema).Append(".");
+        }
+
+        expressionPrinter.Append(Name);
+        expressionPrinter.Append(" VIEW ").Append(IndexName);
+
+        if (Alias != null)
+        {
+            expressionPrinter.Append(" AS ").Append(Alias);
+        }
+    }
 
     protected override TableExpressionBase WithAnnotations(IReadOnlyDictionary<string, IAnnotation> annotations) => throw new NotImplementedException();
 
-    public bool Equals(YdbIndexTableExpression? other) => throw new NotImplementedException();
-    
-    public YdbIndexTableExpression Update(string? alias)
-        => ReferenceEquals(alias, Alias)
-            ? this
-            : new YdbIndexTableExpression(Name, Schema, IndexName, alias);
+    public bool Equals(YdbIndexTableExpression? other) => ReferenceEquals(this, other)
+           || (other is not null
+               && base.Equals(other)
+               && Name == other.Name
+               && Schema == other.Schema
+               && IndexName == other.IndexName);
 
+    protected override Expression VisitChildren(ExpressionVisitor visitor)
+        => WithAlias(Alias);
+
+    public override string ToString()
+        => $"{(!string.IsNullOrEmpty(Schema) ? Schema + "." : "")}{Name} VIEW {IndexName}{(Alias != null ? " AS " + Alias : "")}";
 }
